e40724e1f2ee1d68d89061b838b05b742f3122ef
Chris <orthagonal1@gmail.com>
2018-03-15 10:02:10 -0500

    test start/stop together

--- test/constructor.test.js
+++ test/constructor.test.js
@@ -2,7 +2,7 @@ const tap = require('tap');
 const Queue = require('../');
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/queue';
-/*
+
 tap.test('constructor - mongo required', (t) => {
   t.throws(() => {
     new Queue();
@@ -18,27 +18,47 @@ tap.test('constructor - mongo collection required', (t) => {
 
   t.end();
 });
-*/
-tap.test('constructor - mongo connects', async (t) => {
+
+tap.test('constructor - mongo connects and disconnects', async (t) => {
   const q = new Queue(mongoUrl, 'queue');
   await q.connect();
+  await new Promise(resolve => setTimeout(resolve, 200));
 
   t.type(q.conn, 'object', 'db setup');
   t.type(q.db, 'object', 'db setup');
+  await q.close();
   await new Promise(resolve => setTimeout(resolve, 200));
+  t.type(q.conn, null, 'db closed');
+  t.type(q.db, null, 'db closed');
   t.end();
 });
 
-tap.test('constructor - mongo disconnects', async (t) => {
+tap.test('constructor - can connect and then start later', async (t) => {
   const q = new Queue(mongoUrl, 'queue');
   await q.connect();
   await new Promise(resolve => setTimeout(resolve, 200));
 
-  // t.type(q.conn, 'object', 'db setup');
-  // t.type(q.db, 'object', 'db setup');
+  t.type(q.conn, 'object', 'db setup');
+  t.type(q.db, 'object', 'db setup');
+  await q.start();
+  t.equal(q.exiting, false);
   await q.close();
   await new Promise(resolve => setTimeout(resolve, 200));
-  // t.type(q.conn, null, 'db closed');
-  // t.type(q.db, null, 'db closed');
+  t.type(q.conn, null, 'db closed');
+  t.type(q.db, null, 'db closed');
+  t.end();
+});
+
+tap.test('constructor - connects on start and disconnect on stop', async (t) => {
+  const q = new Queue(mongoUrl, 'queue');
+  await q.start();
+  await new Promise(resolve => setTimeout(resolve, 200));
+
+  t.type(q.conn, 'object', 'db setup');
+  t.type(q.db, 'object', 'db setup');
+  await q.stop();
+  await new Promise(resolve => setTimeout(resolve, 200));
+  t.type(q.conn, null, 'db closed');
+  t.type(q.db, null, 'db closed');
   t.end();
 });
