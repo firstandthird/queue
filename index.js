dbda0c1ef6aa3169a45d17158f6c626601f46b26
orthagonal <orthagonal1@gmail.com>
2018-03-14 21:46:28 -0500

    Add duration (#9)
    * duration
    
    * update test
    
    * move test
    
    * add duration

--- index.js
+++ index.js
@@ -177,11 +177,13 @@ class Queue extends EventEmitter {
       await this.jobs[job.name].process.call(this.bound, job.payload, this, job);
       status = job.status = 'completed';
       job.endTime = new Date();
+      job.duration = job.endTime.getTime() - job.startTime.getTime();
       this.emit('finish', job);
     } catch (err) {
       error = JSON.stringify(err, Object.getOwnPropertyNames(err));
       status = job.status = 'failed';
       job.endTime = new Date();
+      job.duration = job.endTime.getTime() - job.startTime.getTime();
       this.emit('failed', job, err);
     }
     this.db.update({
@@ -189,6 +191,7 @@ class Queue extends EventEmitter {
     }, {
       $set: {
         endTime: job.endTime,
+        duration: job.duration,
         status,
         error
       }
